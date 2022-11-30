69f672c83fa1cec32433a33074984fea9f230648
void-echo <Beihai142857@outlook.com>
2022-11-30 14:47:07 +0800

    small small change

--- LAB/try14/test_and_record_time_cost.sh
+++ LAB/try14/test_and_record_time_cost.sh
@@ -6,19 +6,28 @@
 make clean
 make
 
-# then run the program and record the time cost
+# `small` for small test, `big` for big test
+declare isSmallTestOrBigTest="small"
+# declare isSmallTestOrBigTest="big"
+declare exe_name="out"
+
+if [ "$isSmallTestOrBigTest" = "small" ]; then
+    declare -a thread_nums=("1" "2" "4" "8" "12" "16")
+    # declare -a file_names=("./uniformvector-2dim-5h.txt" "./uniformvector-4dim-1h.txt");
+    declare -a file_names=("./uniformvector-2dim-5h.txt")
+
+elif [ "$isSmallTestOrBigTest" = "big" ]; then
+    declare -a thread_nums=("1" "2" "4" "8" "12" "16" "32" "64")
+    # declare -a file_names=("./uniformvector-2dim-5h.txt" "./uniformvector-4dim-1h.txt");
+    declare -a file_names=("./uniformvector-2dim-5h.txt" "./uniformvector-4dim-1h.txt")
+
+fi
 
 # run ./out and record the time cost.
 # time cost line is the 5th line of the output.
-declare -a thread_nums=("1" "2" "4" "8" "12" "16");
-# declare -a file_names=("./uniformvector-2dim-5h.txt" "./uniformvector-4dim-1h.txt");
-declare -a file_names=("./uniformvector-2dim-5h.txt");
-
-for i in "${file_names[@]}"
-do
-    for j in "${thread_nums[@]}"
-    do
+for i in "${file_names[@]}"; do
+    for j in "${thread_nums[@]}"; do
         echo "file name: $i, thread num: $j"
-        ./out -f $i -t $j | sed -n '5p'
+        ./$exe_name -f $i -t $j | sed -n '5p'
     done
 done
