45c85fdfea84163e7d0f70b97d18817ed9ebd06d
void-echo <beihai142857@outlook.com>
2022-12-20 14:54:42 +0800

    final update

---
+++ LAB/try19/pivot.c
@@ -0,0 +1,447 @@
+#include <immintrin.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <getopt.h>
+#include <sys/time.h>
+#endif
+#ifdef _WIN32
+int gettimeofday(struct timeval *tp, void *tzp) {
+    time_t clock;
+    struct tm tm;
+    SYSTEMTIME wtm;
+    GetLocalTime(&wtm);
+    tm.tm_year = wtm.wYear - 1900;
+    tm.tm_mon = wtm.wMonth - 1;
+    tm.tm_mday = wtm.wDay;
+    tm.tm_hour = wtm.wHour;
+    tm.tm_min = wtm.wMinute;
+    tm.tm_sec = wtm.wSecond;
+    tm.tm_isdst = -1;
+    clock = mktime(&tm);
+    tp->tv_sec = clock;
+    tp->tv_usec = wtm.wMilliseconds * 1000;
+    return (0);
+}
+int opterr = 1, /* if error message should be printed */
+    optind = 1, /* index into parent argv vector */
+    optopt,     /* character checked for validity */
+    optreset;   /* reset getopt */
+char *optarg;   /* argument associated with option */
+
+#define BADCH (int)'?'
+#define BADARG (int)':'
+#define EMSG ""
+
+/*
+ * getopt --
+ *      Parse argc/argv argument vector.
+ * I wrote this code for use of `getopt` on Windows platform.
+ * If it is on Linux, `getopt` is already defined.
+ */
+int getopt(int nargc, char *const nargv[], const char *ostr) {
+    static char *place = EMSG; /* option letter processing */
+    const char *oli;           /* option letter list index */
+
+    if (optreset || !*place) { /* update scanning pointer */
+        optreset = 0;
+        if (optind >= nargc || *(place = nargv[optind]) != '-') {
+            place = EMSG;
+            return (-1);
+        }
+        if (place[1] && *++place == '-') { /* found "--" */
+            ++optind;
+            place = EMSG;
+            return (-1);
+        }
+    } /* option letter okay? */
+    if ((optopt = (int)*place++) == (int)':' || !(oli = strchr(ostr, optopt))) {
+        /*
+         * if the user didn't specify '-' as an option,
+         * assume it means -1.
+         */
+        if (optopt == (int)'-') return (-1);
+        if (!*place) ++optind;
+        if (opterr && *ostr != ':')
+            (void)printf("illegal option -- %c\n", optopt);
+        return (BADCH);
+    }
+    if (*++oli != ':') { /* don't need argument */
+        optarg = NULL;
+        if (!*place) ++optind;
+    } else {        /* need an argument */
+        if (*place) /* no white space */
+            optarg = place;
+        else if (nargc <= ++optind) { /* no arg */
+            place = EMSG;
+            if (*ostr == ':') return (BADARG);
+            if (opterr)
+                (void)printf("option requires an argument -- %c\n", optopt);
+            return (BADCH);
+        } else /* white space */
+            optarg = nargv[optind];
+        place = EMSG;
+        ++optind;
+    }
+    return (optopt); /* dump back option letter */
+}
+#endif
+#include <omp.h>
+
+static int thread_count = 6;
+static double *points;
+static int n, dim, nums_p, p_dot_id;
+#define INLINE inline
+
+// #define INLINE inline __attribute__((always_inline))
+// for gcc, this is right, but VSCode cannot recognize it, and mark it as
+// error! 😰
+
+// n : number of points,
+
+// dim : dimension of points,
+// nums_p : number of pivots,
+// p_dot_id : pivot dot id
+
+static int m = 2;
+static int count = 0;
+static double **euclidean_distance;
+
+typedef struct {
+    int *values;
+    double cost;
+} combination;
+
+static combination *object;
+static double ***cache_eu_dist;  // size: thread_count * m * n
+
+// return the original Euclidean coordinate of the point
+static INLINE double get_point_coordinate_of_id_and_dimension(int id,
+                                                              int dimension) {
+    return points[id * dim + dimension];
+}
+// calculate the Euclidean distance between two points, in the original
+// coordinate
+static INLINE double calc_distance(int id1, int id2) {
+    double sum = 0;
+    for (int i = 0; i < dim; i++) {
+        double diff = get_point_coordinate_of_id_and_dimension(id1, i) -
+                      get_point_coordinate_of_id_and_dimension(id2, i);
+        sum += diff * diff;
+    }
+    return sqrt(sum);
+}
+
+static INLINE double get_euclidean_distance(int id1, int id2) {
+    return euclidean_distance[id1][id2];
+}
+
+static INLINE void calcEuclideanDistanceAndStoreInArray() {
+#pragma omp parallel for num_threads(thread_count)
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == j) {
+                euclidean_distance[i][j] = 0;
+            } else {
+                euclidean_distance[i][j] = calc_distance(i, j);
+            }
+        }
+        // printf("calcEuclideanDistanceAndStoreInArray: %d\n", i);
+    }
+}
+static long long int run_times = 0;
+// static INLINE double calcOneChebyshevDistance(int dot_id1, int dot_id2,
+//                                               int tid) {
+//     double max = 0;
+//     int d1 = tid * m * n;
+//     for (int i = 0; i < m; i++) {
+//         int d_1 = d1 + i * n + dot_id1;
+//         int d_2 = d1 + i * n + dot_id2;
+//         double diff = fabs(cache_eu_dist[d_1] - cache_eu_dist[d_2]);
+//         // double diff = 0;
+//         if (diff > max) {
+//             max = diff;
+//         }
+//     }
+//     return max;
+// }
+static INLINE double calcOneChebyshevDistance(int dot_id1, int dot_id2,
+                                              int tid) {
+    double max = 0;
+    for (int i = 0; i < m; i++) {
+        double diff = fabs(cache_eu_dist[tid][i][dot_id1] -
+                           cache_eu_dist[tid][i][dot_id2]);
+        // double diff = 0;
+        if (diff > max) {
+            max = diff;
+        }
+    }
+    return max;
+}
+
+static int __c_n_m = 0;
+static int INLINE c_n_m(int n, int m) {
+    // C(n, m) = n! / (m! * (n - m)!)
+    if (__c_n_m == 0) {
+        int result = 1;
+        for (int i = 1; i <= m; i++) {
+            result = result * (n - i + 1) / i;
+        }
+        __c_n_m = result;
+        return result;
+    }
+    return __c_n_m;
+}
+
+static void INLINE calc_all_combinations_and_store_in_object() {
+    // printf("calc_all_combinations_and_store_in_object\n");
+    int *combi_ = (int *)malloc(sizeof(int) * m);
+    for (int i = 0; i < m; i++) {
+        combi_[i] = i;
+    }
+    int count_ = 0;
+    while (combi_[m - 1] < n) {
+        for (int __i = 0; __i < m; __i++) {
+            object[count_].values[__i] = combi_[__i];
+        }
+        count_++;
+        int t = m - 1;
+        while (t != 0 && combi_[t] == n - m + t) {
+            t--;
+        }
+        combi_[t]++;
+        for (int i = t + 1; i < m; i++) {
+            combi_[i] = combi_[i - 1] + 1;
+        }
+    }
+}
+
+int compare_2_combinations(const void *a, const void *b) {
+    combination *c1 = (combination *)a;
+    combination *c2 = (combination *)b;
+    // compare double type, but return int type
+    if (c1->cost > c2->cost) {
+        return 1;
+    } else if (c1->cost < c2->cost) {
+        return -1;
+    } else {
+        return 0;
+    }
+}
+
+static void INLINE sort_object_array_by_cost(int DEBUG) {
+    // This function is really quick. when n = 500 and m = 2,
+    // it only takes 18 ms to sort 124750 combinations in object array.
+    if (DEBUG) {
+        // DEBUG mode do nothing more than print time cost of sorting.
+        int len = c_n_m(n, m);
+        struct timeval start, end;
+        gettimeofday(&start, NULL);
+        qsort(object, len, sizeof(combination), compare_2_combinations);
+        gettimeofday(&end, NULL);
+        printf("\n\nqsort() time: %ld ms\n",
+               (end.tv_sec - start.tv_sec) * 1000 +
+                   (end.tv_usec - start.tv_usec) / 1000);
+
+    } else {
+        int len = c_n_m(n, m);
+        qsort(object, len, sizeof(combination), compare_2_combinations);
+    }
+}
+
+static INLINE void sniff_object_array() {
+    // This function provides a way to quickly check the object array.
+    // it does not print all combinations, but only print the first 10 and the
+    // last 10 pivot combinations. It is useful when n is large, and m is small.
+    // print first 10 elements in the object array
+    printf("first 10 elements in the object array:\n");
+    for (int i = 0; i < 10; i++) {
+        printf("cost = %lf, values = ", object[i].cost);
+        for (int j = 0; j < m; j++) {
+            printf("%d ", object[i].values[j]);
+        }
+        printf("\n");
+    }
+    // print the last 10 elements in the object array
+    printf("last 10 elements in the object array:\n");
+    for (int i = c_n_m(n, m) - 10; i < c_n_m(n, m); i++) {
+        printf("cost = %lf, values = ", object[i].cost);
+        for (int j = 0; j < m; j++) {
+            printf("%d ", object[i].values[j]);
+        }
+        printf("\n");
+    }
+}
+
+static void INLINE dmp_object_array_to_file() {
+    FILE *fp = fopen("object.txt", "w");
+    // write the last 1000 combinations to file, reverse order
+    int len = c_n_m(n, m);
+    for (int i = len - 1; i >= len - 1000; i--) {
+        for (int j = 0; j < m; j++) {
+            fprintf(fp, "%d", object[i].values[j]);
+            if (j != m - 1) {
+                fprintf(fp, " ");
+            }
+        }
+        fprintf(fp, "\n");
+    }
+    // write the first 1000 combinations to file, normal order
+    for (int i = 0; i < 1000; i++) {
+        for (int j = 0; j < m; j++) {
+            fprintf(fp, "%d", object[i].values[j]);
+            if (j != m - 1) {
+                fprintf(fp, " ");
+            }
+        }
+        fprintf(fp, "\n");
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int DEBUG = 0;  // 0: run the program normally, 1: run some unit tests
+    if (!DEBUG) {
+        char *filename = (char *)"uniformvector-2dim-5h.txt";
+        // usage: ./pivot -f <filename> -t <thread_count>
+        // parse command line arguments
+        int opt;
+        while ((opt = getopt(argc, argv, "f:t:")) != -1) {
+            switch (opt) {
+                case 'f':
+                    filename = optarg;
+                    break;
+                case 't':
+                    thread_count = atoi(optarg);
+                    omp_set_num_threads(thread_count);
+                    break;
+                default:
+                    printf("Usage: ./pivot -f <filename> -t <thread_count>");
+                    return -1;
+            }
+        }
+
+        struct timeval start, end;
+
+        // M : number of combinations to store
+        const int M = 1000;
+        FILE *file = fopen(filename, "r");
+        if (file == NULL) {
+            printf("%s file not found.\n", filename);
+            return -1;
+        }
+        fscanf(file, "%d", &dim);
+        fscanf(file, "%d", &n);
+        fscanf(file, "%d", &nums_p);
+        m = nums_p;
+        printf(
+            "dim = %d, point number = %d, pivot numver = %d, thread number = "
+            "%d, using file %s\n",
+            dim, n, nums_p, thread_count, filename);
+
+        // allocate memory for points
+        points = (double *)malloc(sizeof(double) * n * dim);
+        // cache_eu_dist is double**
+        cache_eu_dist = (double ***)malloc(sizeof(double **) * thread_count);
+        for (int i = 0; i < thread_count; i++) {
+            cache_eu_dist[i] = (double **)malloc(sizeof(double *) * m);
+            for (int j = 0; j < m; j++) {
+                cache_eu_dist[i][j] = (double *)malloc(sizeof(double) * n);
+            }
+        }
+
+        // read points
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < dim; j++) {
+                fscanf(file, "%lf", &points[i * dim + j]);
+            }
+        }
+        printf("points read finished.\n");
+        fclose(file);
+        gettimeofday(&start, NULL);
+        euclidean_distance = (double **)malloc(sizeof(double *) * n);
+        for (int i = 0; i < n; i++) {
+            euclidean_distance[i] = (double *)malloc(sizeof(double) * n);
+        }
+        object = (combination *)malloc(sizeof(combination) * c_n_m(n, m));
+        for (int i = 0; i < c_n_m(n, m); i++) {
+            object[i].values = (int *)malloc(sizeof(int) * m);
+            object[i].cost = 0;
+        }
+        // calculate the Euclidean distance between every two points, in the
+        // original coordinate.
+        calcEuclideanDistanceAndStoreInArray();
+        printf("Euclidean distance calculated.\n");
+        calc_all_combinations_and_store_in_object();
+        printf("all combinations calculated.\n");
+
+        // *********************************************************
+        int len = c_n_m(n, m);
+// set omp thread number
+#pragma omp parallel for num_threads(thread_count)
+        for (int i = 0; i < len; i++) {
+            int tid = omp_get_thread_num();
+            int *values = object[i].values;
+            for (int __m__ = 0; __m__ < m; __m__++) {
+                int pivot_id = values[__m__];
+                cache_eu_dist[tid][__m__] = euclidean_distance[pivot_id];
+            }
+            for (int __i__ = 0; __i__ < n; __i__++) {
+                int bound = __i__ + 1;
+                for (int __j__ = bound; __j__ < n; __j__++) {
+                    object[i].cost +=
+                        calcOneChebyshevDistance(__i__, __j__, tid);
+                }
+            }
+        }
+
+        // sort the object array by cost
+        sort_object_array_by_cost(0);  // 1: show debug message, 0: not
+
+        // end timer
+        gettimeofday(&end, NULL);
+        printf("time cost: %ld ms \n",
+               (end.tv_sec - start.tv_sec) * 1000 +
+                   (end.tv_usec - start.tv_usec) / 1000);
+        // sniff_object_array();  // result seems to be correct :-)
+        dmp_object_array_to_file();
+        free(points);
+        // free euclidean_distance
+        for (int i = 0; i < n; i++) {
+            free(euclidean_distance[i]);
+        }
+        free(euclidean_distance);
+        for (int i = 0; i < c_n_m(n, m); i++) {
+            free(object[i].values);
+        }
+        free(object);
+        for (int i = 0; i < thread_count; i++) {
+            for (int j = 0; j < m; j++) {
+                free(cache_eu_dist[i][j]);
+            }
+            free(cache_eu_dist[i]);
+        }
+        free(cache_eu_dist);
+    } else {
+        struct timeval start, end;
+        gettimeofday(&start, NULL);
+        n = 500;
+        m = 2;
+        object = (combination *)malloc(sizeof(combination) * c_n_m(n, m));
+        for (int i = 0; i < c_n_m(n, m); i++) {
+            object[i].values = (int *)malloc(sizeof(int) * m);
+            object[i].cost = 0;
+        }
+        calc_all_combinations_and_store_in_object();
+        sniff_object_array();
+        gettimeofday(&end, NULL);
+        printf("time cost: %ld ms \n",
+               (end.tv_sec - start.tv_sec) * 1000 +
+                   (end.tv_usec - start.tv_usec) / 1000);
+    }
+    return 0;
+}
--- README.md
+++ README.md
@@ -10,3 +10,100 @@ If you have any questions, please feel free to raise an issue or send an email.
 ⚠ For the interest of academic integrity, please do not copy the code of this repo directly.
 
 Hope I can help you. code and have fun!
+
+## 代码说明
+
+本实验最终代码包含一个文件：pivot.c 
+
+程序中使用了OpenMP进行并行化，运行命令的主机应该支持OpenMP。
+
+gcc 版本大于等于 8.1 的时候工作，代码正常编译运行。对于更低版本的 gcc 或者 Intel OneAPI，并未做过相应的测试。
+
+
+
+## 编译 & 运行
+
+代码在 `源代码` 目录下。为了方便老师进行测试，我们写了 `Makefile`和 `test.sh`脚本。要想进行编译，只需要 `make`一下即可。通过运行 `.\pivot`，就能在默认参数（线程数量：6，小数据集）下运行程序。
+
+如果要测试其他线程数、数据集，只需要在  `.\pivot` 之后加上参数。格式是 `-t <thread_number> -f <file_name>`。两个参数的顺序是自由的，也都是可选的。缺失的参数将被赋予默认值。
+
+程序在运行之后会把结果写入 `object.txt`当中。经过反复验证，不论是大小数据集，我们的输出结果和参考文件完全一致。写入的模式是覆盖式的，所以不必先清空文件内容再测试。
+
+如果要进行测试，可以运行 `test.sh`脚本。它的内容如下所示。它会测试不同线程、不同数据集下的程序耗时并且输出，单位为毫秒。测试也有两个模式：大型测试和小型测试，默认是小型测试。如果想要进行完整（最高 64 核，并且大数据集也在内）的测试，只需要修改第9，10行的内容即可。
+
+```bash
+#!/bin/bash
+
+# This script is used to test the time cost of the program `pivot.c`.
+# first make clean and make the program
+make clean
+make
+
+# `small` for small test, `big` for big test
+declare isSmallTestOrBigTest="small"
+# declare isSmallTestOrBigTest="big"
+declare exe_name="out"
+
+if [ "$isSmallTestOrBigTest" = "small" ]; then
+    declare -a thread_nums=("1" "2" "4" "8" "12" "16")
+    declare -a file_names=("./uniformvector-2dim-5h.txt")
+
+elif [ "$isSmallTestOrBigTest" = "big" ]; then
+    declare -a thread_nums=("1" "2" "4" "8" "12" "16" "32" "64")
+    declare -a file_names=("./uniformvector-2dim-5h.txt" "./uniformvector-4dim-1h.txt")
+
+fi
+
+# run ./out and record the time cost.
+# time cost line is the 5th line of the output.
+for i in "${file_names[@]}"; do
+    for j in "${thread_nums[@]}"; do
+        echo "file name: $i, thread num: $j"
+        ./$exe_name -f $i -t $j | sed -n '5p'
+    done
+done
+```
+
+最后，如果由于平台或其他原因发生了编译错误，可以在 `prepared_bin` 文件夹里面找到我们预先编译好的二进制文件。一般情况下不需要这个文件，它只是用来防止意外情况。
+
+
+
+我们的代码在一次编译之后，多次运行速度有所波动。如果条件允许，请老师多次测试取性能最好值。
+
+
+
+## 之前的性能测试
+
+我们依托依托山东大学高性能计算平台进行过测试，其配置信息如下：
+
+- 操作系统：CentOS Linux release 7.8.2003 (Core)
+- CPU：Intel (R) Xeon (R) Gold 6248 CPU @ 2.50GHz
+- 内存：4GB
+
+准确性能以实验室测试平台为准。该结果仅供参考。
+
+#### 1.2.1 测试数据“uniformvector-2dim-5h.txt”
+
+| **线程数量** | **执行时间 (ms)** | **加速比** |
+| ------------ | ----------------- | ---------- |
+| 1            | 28790             | 1          |
+| 2            | 14552             | 1.98       |
+| 4            | 7643              | 3.77       |
+| 8            | 3897              | 7.39       |
+| 12           | 2694              | 10.68      |
+
+<img src="https://cdn.nlark.com/yuque/0/2022/png/34593176/1670058488655-b3596d5e-352f-4d85-8594-05a19bee284b.png#averageHue=%23fdfdfd&clientId=u269e801e-ea98-4&crop=0&crop=0&crop=1&crop=1&from=drop&height=356&id=u06d85a03&margin=%5Bobject%20Object%5D&name=1%281%29.png&originHeight=847&originWidth=852&originalType=binary&ratio=1&rotation=0&showTitle=false&size=23328&status=done&style=none&taskId=u5739fc5e-82be-40a8-99a5-4b97f01e2ab&title=&width=358" alt="1(1).png" style="zoom: 33%;" /><img src="https://cdn.nlark.com/yuque/0/2022/png/34593176/1670058488709-3a9bbe34-57e3-434d-a05d-a91c59dd622c.png#averageHue=%23fdfdfd&clientId=u269e801e-ea98-4&crop=0&crop=0&crop=1&crop=1&from=drop&height=351&id=uc657a609&margin=%5Bobject%20Object%5D&name=2%281%29.png&originHeight=825&originWidth=815&originalType=binary&ratio=1&rotation=0&showTitle=false&size=20341&status=done&style=none&taskId=u6cb5c2fa-923a-468c-9598-548363cc40a&title=&width=347" alt="2(1).png" style="zoom:33%;" />
+
+#### 1.2.2  测试数据“uniformvector-4dim-1h.txt”
+
+| **线程数量** | **执行时间 (ms)** | **加速比** |
+| ------------ | ----------------- | ---------- |
+| 1            | 1416201           | 1          |
+| 2            | 685185            | 2.06       |
+| 4            | 371142            | 3.82       |
+| 8            | 245492            | 5.77       |
+| 12           | 191242            | 7.40       |
+
+<img src="https://cdn.nlark.com/yuque/0/2022/png/34593176/1670071841586-ab7ee5a1-cd51-423e-8e77-b4ee607a00c5.png#averageHue=%23fdfdfd&clientId=ud51690d5-9395-4&crop=0&crop=0&crop=1&crop=1&from=paste&height=363&id=u55702652&margin=%5Bobject%20Object%5D&name=%7B06BCF299-BFCD-77FD-2FCE-B40FAE87BB7C%7D.png&originHeight=856&originWidth=851&originalType=binary&ratio=1&rotation=0&showTitle=false&size=35555&status=done&style=none&taskId=uc832d673-76a8-4adc-973c-b57509445a9&title=&width=360.7875061035156" alt="{06BCF299-BFCD-77FD-2FCE-B40FAE87BB7C}.png" style="zoom:33%;" /><img src="https://cdn.nlark.com/yuque/0/2022/png/34593176/1670071845141-d612e1fb-df09-4de0-8042-33ff5f3696b1.png#averageHue=%23fdfdfd&clientId=ud51690d5-9395-4&crop=0&crop=0&crop=1&crop=1&from=paste&height=349&id=u1272354f&margin=%5Bobject%20Object%5D&name=%7B2424CE7E-65C7-4987-19CF-01E183C89EE3%7D.png&originHeight=835&originWidth=820&originalType=binary&ratio=1&rotation=0&showTitle=false&size=33927&status=done&style=none&taskId=u7d3f44d4-392d-4943-aed6-49db81f90ae&title=&width=343" alt="{2424CE7E-65C7-4987-19CF-01E183C89EE3}.png" style="zoom:33%;" />
+
+总体来看，我们的性能还算不错，但是也不算很快。不过，我们在小数据集上取得了几乎线性的加速比增长（至少在核数不太大的时候）。
